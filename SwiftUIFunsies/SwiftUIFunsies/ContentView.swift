92cec40b8fff8041104490672558cd4c5e348942
marinadjundjas <djundjas@sipgate.de>
2022-04-12 13:14:37 +0200

    :heavy_plus_sign: Added Navigation, styled the ListItems

--- SwiftUIFunsies/SwiftUIFunsies/ContentView.swift
+++ SwiftUIFunsies/SwiftUIFunsies/ContentView.swift
@@ -2,8 +2,119 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List() {
+                
+                Section() {                NavigationLink (destination: RandomView()) {
+                    HStack {
+                        Image("unknown")
+                            .frame(width: 48, height: 48)
+                        //TODO: image gotta be fitting in the circle
+                        .background(.orange)
+                        .clipShape(RoundedRectangle(cornerRadius: 100))
+                        .foregroundColor(.white)
+                    
+                   VStack {
+                       Text("Marina")
+                           .frame(maxWidth:.infinity, alignment: .leading)
+                       Text("djgjkdfgkdfgd")
+                           .frame(maxWidth:.infinity, alignment: .leading)
+                   }
+                   .padding(.vertical, 16)
+                    .padding(.horizontal, 8)}}
+                }
+                
+                Section(header: Text("Account")) {                NavigationLink (destination: RandomView()) {
+                    HStack {
+                    Image (systemName: "key")
+                            .frame(width: 32, height: 32)
+                        .background(.orange)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .foregroundColor(.white)
+                    
+                    Text("Login")
+                        .padding(.vertical, 16)
+                        .padding(.horizontal, 8)
+                }}
+                    NavigationLink (destination: RandomView()) {            HStack {
+                        Image (systemName: "plus")
+                            .frame(width: 32, height: 32)
+                            .background(.orange)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                            .foregroundColor(.white)
+                        
+                        Text("satellite Plus")
+                            .padding(.vertical, 16)
+                            .padding(.horizontal, 8)
+                    }}
+                    
+                    NavigationLink (destination: RandomView()) {            HStack {
+                        Image (systemName: "case")
+                            .frame(width: 32, height: 32)
+                            .background(.orange)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                            .foregroundColor(.white)
+                        
+                        Text("satellite Business")
+                            .padding(.vertical, 16)
+                            .padding(.horizontal, 8)
+                    }}
+                }
+                
+                Section(header: Text("Benachrichtigungen")) {                NavigationLink (destination: RandomView()) {
+                    HStack {
+                    Image (systemName: "bell.badge")
+                            .frame(width: 32, height: 32)
+                        .background(.orange)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .foregroundColor(.white)
+                    
+                    Text("Mitteilungen")
+                        .padding(.vertical, 16)
+                        .padding(.horizontal, 8)
+                }}
+                    NavigationLink (destination: RandomView()) {            HStack {
+                        Image (systemName: "person.badge.clock")
+                            .frame(width: 32, height: 32)
+                            .background(.orange)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                            .foregroundColor(.white)
+                        
+                        Text("Erreichbarkeit")
+                            .padding(.vertical, 16)
+                            .padding(.horizontal, 8)
+                    }}
+                }
+                
+                Section(header: Text("Support")) {                NavigationLink (destination: RandomView()) {
+                    HStack {
+                    Image (systemName: "questionmark")
+                            .frame(width: 32, height: 32)
+                        .background(.blue)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .foregroundColor(.white)
+                    
+                    Text("Häufig gestellte Fragen")
+                        .padding(.vertical, 16)
+                        .padding(.horizontal, 8)
+                }}
+                    NavigationLink (destination: RandomView()) {            HStack {
+                        Image (systemName: "message")
+                            .frame(width: 32, height: 32)
+                            .background(.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                            .foregroundColor(.white)
+                        
+                        Text("Support Chat")
+                            .padding(.vertical, 16)
+                            .padding(.horizontal, 8)
+                    }}
+                }
+                
+            }
+            
+        }
+        
     }
 }
 
---
+++ SwiftUIFunsies/SwiftUIFunsies/RandomView.swift
@@ -0,0 +1,15 @@
+
+
+import SwiftUI
+
+struct RandomView: View {
+    var body: some View {
+        Text("Das ist die Random View!")
+    }
+}
+
+struct RandomView_Previews: PreviewProvider {
+    static var previews: some View {
+        RandomView()
+    }
+}
