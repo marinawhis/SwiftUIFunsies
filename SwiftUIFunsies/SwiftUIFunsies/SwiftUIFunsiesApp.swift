38a2178ad4016e4ec75a11ae201e0dcaa46542d1
marinadjundjas <djundjas@sipgate.de>
2022-04-12 10:38:50 +0200

    Created project

---
+++ SwiftUIFunsies/SwiftUIFunsies/SwiftUIFunsiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFunsiesApp.swift
+//  SwiftUIFunsies
+//
+//  Created by Marina Djundjas on 12.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFunsiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
